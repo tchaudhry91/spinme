ae18c3a8649b93941a6354b95ebfae8ab17b445d
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2019-08-14 18:53:59 +0000

    Fix missing doc

--- cmd/down.go
+++ cmd/down.go
@@ -28,13 +28,7 @@ var id string
 // downCmd represents the down command
 var downCmd = &cobra.Command{
 	Use:   "down",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Bring down the given container",
 	Run: func(cmd *cobra.Command, args []string) {
 		err := spin.SlashID(context.Background(), id)
 		if err != nil {
